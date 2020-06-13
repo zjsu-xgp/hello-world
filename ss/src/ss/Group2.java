72821f29e53b41fa5dbb7af1d7b10bb998f44aee
XGP <XGP@LAPTOP-31U3QDK1>
2020-06-13 21:21:59 +0800

    modify Group2.java

--- ss/src/ss/Group2.java
+++ ss/src/ss/Group2.java
@@ -4,7 +4,7 @@ public class Group2 {
 
 	public static void main(String[] args) {
 		String monday = "Monday";
-		String tuesday = "December";
+		String tuesday = "Tuesday";
 		String sunday = "Sunday";
 		String friday = "Friday";
 		String thursday = "Thursday";
