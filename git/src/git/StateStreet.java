0e228ac4c1f889946d6c46f6c6b4ef320a09c7af
XGP <XGP@LAPTOP-31U3QDK1>
2020-06-13 15:03:13 +0800

    upload project

---
+++ git/src/git/StateStreet.java
@@ -0,0 +1,14 @@
+package git;
+
+public class StateStreet {
+
+	public static void main(String[] args) {
+		
+		//Eclipse Git stashes的用法演示
+		
+		String monday = "Monday";
+		String tuesday = "Tuesday";
+		String wednesday = "星期三";
+	}
+
+}
