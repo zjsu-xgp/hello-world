d7bfc4bbceebd81bc6788c9e4a6133577d4272d9
XGP <XGP@LAPTOP-31U3QDK1>
2020-06-14 17:45:37 +0800

    release

--- ss/src/test/Master.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Master {
-
-	public static void main(String[] args) {
-		System.out.print("This is master branch");
-	}
-
-}
--- ss/src/test/Test.java
+++ ss/src/test/Test.java
@@ -1,15 +1,12 @@
 package test;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
 
-		int a = 5;
-		System.out.print("test");
-
-		System.out.println("This is a test");
-
-		int g = 67;
+		Date date = new Date();
 
 
 
