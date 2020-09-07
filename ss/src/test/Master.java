fe76287766af246fb4f65a6460ce5357dad50ef0
XGP <XGP@LAPTOP-31U3QDK1>
2020-09-07 22:55:31 +0800

    modify master

--- ss/src/test/Master.java
+++ ss/src/test/Master.java
@@ -3,9 +3,12 @@ package test;
 public class Master {
 
 	public static void main(String[] args) {
-		int b = 66;
-		System.out.print("hello");
+		int b = 99;	
 
+	}
+	private int zero() {
+		return 0;
+		
 	}
 
 }
