075c159236849f5065252e443d14b9829db5939d
zjsu-xgp <62364579+zjsu-xgp@users.noreply.github.com>
2020-09-09 20:34:09 +0800

    Update Fix.java

--- ss/src/test/Fix.java
+++ ss/src/test/Fix.java
@@ -5,9 +5,11 @@ public class Fix {
 	public static void main(String[] args) {
 
 
-		System.out.println("master");
+		System.out.println("add new ");
 
 		System.out.println("release108");
+
+                
 
 
 	}
