8fcf3417e746795669aaba4a7c1d41b6be0763d3
XGP <XGP@LAPTOP-31U3QDK1>
2020-09-09 21:18:17 +0800

    commit to local

--- ss/src/test/Fix.java
+++ ss/src/test/Fix.java
@@ -3,9 +3,11 @@ package test;
 public class Fix {
 
 	public static void main(String[] args) {
-
-	  System.out.println("modify");
-          String a = "string 123";
+		 int b = 6;
+		
+	     System.out.println("modify");
+	     
+         String a = "string 123";
 
 	}
 }
